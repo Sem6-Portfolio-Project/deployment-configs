a0088e468581a7b3497629a2e48394e683922718
Neranjan Pushpakumara <neranjan@aayutechnologies.com>
2025-04-08 21:31:05 +0530

    refactored the code for the better flow

--- scripts/bus-management.sh
+++ scripts/bus-management.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-source "./utils.sh"
+source './utils.sh'
 
 # This is distribution destination
-DIST="$AUTH_BACKEND/dist"
+DIST="$BUS_MANAGE_BACKEND/dist"
 
-ZIP_FILE_NAME_BACKEND='build_backend_auth'
-S3_DIR_LAMBDA_CODE="$S3_LAMBDA_COMMON_PATH/backend-auth"
-TEMPLATE_NAME="user-management.yaml"
+ZIP_FILE_NAME_BACKEND='build_backend_bus_manage'
+S3_DIR_LAMBDA_CODE="$S3_LAMBDA_COMMON_PATH/backend-bus-manage"
+TEMPLATE_NAME="bus-management.yaml"
 
-build_and_upload_auth_backend_code() {
-  echo "Building the auth backend code..."
-  # Build the auth backend and create the zip folder
+build_and_upload_bus_manage_backend_code() {
+  echo "Building the bus management backend code..."
+  # Build the bus management backend and create the zip folder
   build_backend_and_create_zip "$DIST" "$AUTH_BACKEND" "$ZIP_FILE_NAME_BACKEND"
 
-  echo "uploading the auth backend code..."
+  echo "Uploading the bus management backend code..."
   # Upload build zip file to the S3 bucket.
   upload_build_project_to_s3 "$DIST" "$ZIP_FILE_NAME_BACKEND.zip" "$S3_DIR_LAMBDA_CODE"
 }
 
-upload_and_validate_auth_cf_template() {
+upload_and_validate_bus_manage_cf_template() {
   echo "Uploading the auth cf template..."
   # Upload the CF template to the s3 bucket
   upload_cf_template "$TEMPLATE_DIR" "$TEMPLATE_NAME" "$S3_TEMPLATES_COMMON_PATH"
@@ -28,4 +28,3 @@ upload_and_validate_auth_cf_template() {
   # validating the cf template
   validate_cf_template "$S3_DOMAIN_URL/$RESOURCE_BUCKET_NAME/$S3_TEMPLATES_COMMON_PATH/$TEMPLATE_NAME"
 }
-
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -14,6 +14,7 @@ export PROJECT_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null &&
 
 #Project directories
 export AUTH_BACKEND="$PROJECT_ROOT/mobile-app-backend-auth"
+export BUS_MANAGE_BACKEND="$PROJECT_ROOT/bus-tracking-and-scheduling"
 export LAYERS_DIR="$PROJECT_ROOT/layers"
 
 #Template directories
@@ -47,6 +48,116 @@ echo_success() {
     echo -e "\033[32m$1\033[0m"
 }
 
+# This will build the project and create zip file contains the source code
+#      dist=$1 : Distribution destination ( build files store here)
+#      backend=$2 : Path to the backend project
+#      zip_file_name=$3 : Name of the build zip file
+
+build_backend_and_create_zip() {
+    dist=$1
+    backend=$2
+    zip_file_name=$3
+
+    set -e
+
+    echo "Building auth backend ZIP..."
+
+    mkdir -p "$dist/"
+    rm -rf "$dist"
+    cd "$backend"
+
+    echo "Updating dependencies..."
+    npm ci
+
+    echo "Compiling Typescript code to Javascript..."
+    npx tsc
+
+    echo $(git rev-parse --short=7 HEAD) \
+    $([[ "$(git status --porcelain --untracked-files=no)" == "" ]] && echo "ok" || echo "tainted") \
+    $USERNAME@${HOSTNAME:-$COMPUTERNAME} $(date +%s000) \
+    > dist/BUILD
+
+    cd -
+
+    cd "$dist" && zip -9vr "$zip_file_name"\
+      lib/ ../package.json ../package-lock.json BUILD
+
+    cd -
+    set +e
+}
+
+# Synchronize specific file from local source directory to s3 bucket.
+# params
+#   $1 - The local source directory.
+#   $2 - The name of the file to upload.
+#   $3 - The destination directory in the S3 bucket.
+s3_sync() {
+  local source_dir=$1
+  local source_key=$2
+  local dest_dir=$3
+
+#  echo "$source_dir"
+#  echo "$source_key"
+#  echo "$dest_dir"
+
+  if [[ -z "$source_dir" || -z "$source_key" || -z "$dest_dir" ]]; then
+    echo_error "Error: Missing arguments"
+    echo_error "Usage: s3_sync <source_dir> <source_key> <des_dir>"
+    exit 1
+  fi
+
+  echo "Uploading $source_key to S3 bucket $dest_dir"
+  aws s3 sync --profile "$AWS_PROFILE" \
+    "$source_dir" "s3://$RESOURCE_BUCKET_NAME/$dest_dir" \
+    --exclude="*" --include="$source_key" || echo_error "Error while uploading $source_key."
+
+#  aws s3 ls s3://$RESOURCE_BUCKET_NAME/ --recursive --human-readable --summarize --profile "$AWS_PROFILE"
+}
+
+# This will upload build zip file to the S3 bucket.
+#    dist=$1 : Distribution directory ( build files directory )
+#    zip_file_name=$2
+#    s3_dir_lambda_code=$3 : s3 url for the code
+upload_build_project_to_s3() {
+  dist=$1
+  zip_file_name=$2
+  s3_dir_lambda_code=$3
+  s3_sync "$dist" "$zip_file_name.zip" "$s3_dir_lambda_code"
+}
+
+# This will upload the CF templates
+#    template_dir=$1 : local directory of the template
+#    template_name=$2 : template name
+#    s3_template_common_path=$3 : s3 url for the template directory
+upload_cf_template() {
+  template_dir=$1
+  template_name=$2
+  s3_template_common_path=$3
+  s3_sync "$template_dir" "$template_name" "$s3_template_common_path"
+}
+
+# This will validate the cf template
+validate_cf_template() {
+  template_s3_url=$1
+  echo "Validating the $template_s3_url template."
+
+  output=$(
+    aws cloudformation validate-template \
+      --profile "$AWS_PROFILE" \
+      --region "$AWS_REGION"\
+      --template-url "$template_s3_url" 2>&1
+   )
+
+  if [[ $? -eq 0 ]]; then
+    echo_success "Successfully validated the CloudFormation template."
+    exit 0
+  else
+    echo_error "Validation error for template:"
+    echo_error "$template_s3_url"
+    echo_error "$output"
+    exit 1
+  fi
+}
 
 
 
