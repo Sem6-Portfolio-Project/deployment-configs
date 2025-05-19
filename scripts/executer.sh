90eff59603bfa706a219eba4f875a5a28ba00f48
Neranjan Pushpakumara <neranjan@aayutechnologies.com>
2025-05-19 14:33:38 +0530

    added update lambdas

--- scripts/executer.sh
+++ scripts/executer.sh
@@ -4,6 +4,7 @@ source './utils.sh'
 source './user-management.sh'
 source './stack-deployment.sh'
 source './bus-management.sh'
+source './general-services.sh'
 
 echo "OPTIONS"
 echo "1.Deploy"
@@ -23,8 +24,9 @@ runner() {
     echo "DEPLOYMENT OPTIONS"
     echo "1.Deploy User-management stack"
     echo "2.Deploy Bus-management stack"
-    echo "3.Deploy main-stack-integration stack"
-    echo "4.All"
+    echo "3.Deploy General-services stack"
+    echo "4.Deploy main-stack-integration stack"
+    echo "5.All"
 
     echo "PLEASE NOTE THAT IF YOU ALREADY DIDN'T DEPLOY THE COMMON STACK , PLEASE DEPLOY IT FIRST BY RUNNING 'common.sh'"
     echo -n "Please enter option : "
@@ -48,12 +50,18 @@ runner() {
       upload_and_validate_bus_manage_cf_template
       PARAMS=$(stack_params_for_bus_management)
       deploy_cf_stack "bus-management" "bus-management.yaml" "$PARAMS"
-    elif [[ "$input" = 3 ]]; then
+    elif [[ "$input" = 3 ]] ;then
+      echo "Deploying general services stack..."
+      build_and_upload_general_services_hub
+      upload_and_validate_general_service_cf_template
+      PARAMS=$(stack_params_for_general_services_stack)
+      deploy_cf_stack "general-service-hub" "general-service.yaml" "$PARAMS"
+    elif [[ "$input" = 4 ]]; then
       echo "Deploying main stack integration stack..."
       upload_and_validate_main_stack_integration_cf_template
       PARAMS=$(stack_params_for_main_stack_integration)
       deploy_cf_stack "main-stack-integration" "main-stack-integration.yaml" "$PARAMS"
-    elif [[ "$input" = 4 ]]; then
+    elif [[ "$input" = 5 ]]; then
       echo "Deploying user management stack..."
       build_and_upload_auth_backend_code
       upload_and_validate_auth_cf_template
@@ -65,6 +73,12 @@ runner() {
       upload_and_validate_bus_manage_cf_template
       PARAMS=$(stack_params_for_bus_management)
       deploy_cf_stack "bus-management" "bus-management.yaml" "$PARAMS"
+
+      echo "Deploying general services stack..."
+      build_and_upload_general_services_hub
+      upload_and_validate_general_service_cf_template
+      PARAMS=$(stack_params_for_general_services_stack)
+      deploy_cf_stack "general-service-hub" "general-service.yaml" "$PARAMS"
 
       echo "Deploying main stack integration stack..."
       upload_and_validate_main_stack_integration_cf_template
@@ -78,7 +92,9 @@ runner() {
     echo "3.Update WSConnect lambdas"
     echo "4.Update WSDisconnect lambdas"
     echo "5.Update WSSendLocation lambdas"
-    echo "6.All lambdas"
+    echo "6.Update Notification invoker lambda"
+    echo "7.Update general service lambda"
+    echo "8.All lambdas"
 
     echo -n "Please enter option : "
     read input
@@ -104,7 +120,13 @@ runner() {
     elif [[ "$input" = 5 ]] ;then
       echo "Updating WSSendLocation lambdas..."
       update_WSSendLocation_lambda
-    elif [[ "$input" = 6 ]]; then
+    elif [[ "$input" = 6 ]] ;then
+      echo "Updating Notification invoker lambda..."
+      update_notification_invoker_lambda
+    elif [[ "$input" = 7 ]] ;then
+      echo "Updating general service lambda..."
+      update_general_service_lambda
+    elif [[ "$input" = 8 ]]; then
       echo "Updating user management lambdas..."
       update_auth_lambda
 
@@ -119,6 +141,12 @@ runner() {
 
       echo "Updating WSSendLocation lambdas..."
       update_WSSendLocation_lambda
+
+      echo "Updating Notification invoker lambda..."
+      update_notification_invoker_lambda
+
+      echo "Updating general service lambda..."
+      update_general_service_lambda
     fi
   fi
 }
